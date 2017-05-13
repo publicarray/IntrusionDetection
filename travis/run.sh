7500ef2369649169d5ae127e957031214ebd35b2
Sebastian Schmidt <publicarray@icloud.com>
2017-05-13 20:51:52 +1000

    force ln

--- travis/run.sh
+++ travis/run.sh
@@ -5,6 +5,6 @@
 touch "asm/folder 1/bash_profile"
 echo "hEL" > "asm/folder 1/bashrc"
 chmod +x "asm/folder 1/editorconfig"
-ln -s "asm/folder 1/zshrc" asm/folder2/symlink
+ln -sf "asm/folder 1/zshrc" asm/folder2/symlink
 
 ./main.sh -f -v result.txt
